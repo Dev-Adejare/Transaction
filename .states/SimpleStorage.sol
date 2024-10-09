0f005413c158a770644bc00569d4d07731cf511b
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-09 11:55:32 +0100

    Update simplestorage

--- .states/SimpleStorage.sol
+++ .states/SimpleStorage.sol
@@ -1,34 +1,51 @@
 // SPDX-License-Identifier: MIT
+// This line specifies the license type for the contract, which is "MIT" in this case. 
+// SPDX is a standardized way of declaring software licenses in source code.
+
 pragma solidity ^0.8.27;
+// This line specifies the version of the Solidity compiler that should be used. 
+// The contract is written for Solidity version 0.8.27.
 
 contract SimpleStorage {
+    // Define a public state variable "vaultNumber" of type uint32 (unsigned 32-bit integer).
     uint32 public vaultNumber;
 
+    // A function named "setNum" that takes a uint32 input "_num" and sets "vaultNumber" to this value.
+    // The function is public (can be called from outside the contract) and returns a boolean value (true).
     function setNum(uint32 _num) public returns (bool) {
-        vaultNumber = _num;
-        return true;
+        vaultNumber = _num; // Set the vaultNumber to the input value "_num".
+        return true;        // Return true to indicate the operation was successful.
     }
     
-    //Getter function which returns all the argument we pass in the function
+    // A getter function named "getNum" that returns two uint32 values: "num_" and "vatN_".
+    // The "view" keyword means it doesn't modify the blockchain state (no gas fees for calling it).
     function getNum() public view returns (uint32 num_, uint32 vatN_) {
-        num_ = vaultNumber;
-        uint32 vat = 50;
+        num_ = vaultNumber; // Set "num_" to the value of the state variable "vaultNumber".
+        uint32 vat = 50;    // Define a local uint32 variable "vat" and set it to 50.
+        
+        // Call the private helper function "calculateReward" with "vaultNumber" and "vat" as inputs.
+        // The function returns three values, but we only care about the first (mul_) and third (div_).
         (uint32 mul_, , uint32 div_) = calculateReward(vaultNumber, vat);
+        
+        // Set "vatN_" to the sum of "mul_" (vaultNumber * vat) and "div_" (vaultNumber / vat).
         vatN_ = mul_ + div_;
     }
 
-    // This private function make is an Helper method which does not expose the code to the public
+    // A private helper function named "calculateReward" that takes two uint32 inputs: "_vaultNum" and "_vat".
+    // The function returns three uint32 values: "mul_", "add_", and "div_".
+    // The "pure" keyword indicates that the function doesn't read or modify the contract's state, only performs computations.
     function calculateReward(uint32 _vaultNum, uint32 _vat)
         private
         pure
         returns (
-            uint32 mul_,
-            uint32 add_,
-            uint32 div_
+            uint32 mul_, // Product of "_vaultNum" and "_vat".
+            uint32 add_, // Sum of "_vaultNum" and "_vat".
+            uint32 div_  // Division of "_vaultNum" by "_vat".
         )
     {
-        mul_ = _vaultNum * _vat;
-        add_ = _vaultNum + _vat;
-        div_ = _vaultNum / _vat;
+        // Calculate and assign values to the return variables.
+        mul_ = _vaultNum * _vat;  // Multiply "_vaultNum" by "_vat".
+        add_ = _vaultNum + _vat;  // Add "_vaultNum" and "_vat".
+        div_ = _vaultNum / _vat;  // Divide "_vaultNum" by "_vat" (integer division).
     }
 }
--- .states/Todo.sol
+++ .states/Todo.sol
@@ -1,4 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-contract Todo 
+// Define a contract named "TodoList"
+contract TodoList {
+    
+    // Declare a public variable "owner" of type address, which will hold the address of the contract owner.
+    address public owner;
+
+    // Define an enumeration (enum) named "Status" with four possible states for a todo: None, Created, Edited, and Done.
+    enum Status { None, Created, Edited, Done }
+
+    // Define a struct named "Todo" that represents a task. 
+    // It includes a title (string), a description (string), and a status (from the Status enum).
+    struct Todo {
+        string title;
+        string description;
+        Status status;
+    }
+
+    // Declare a public dynamic array of "Todo" structs named "todos".
+    Todo[] public todos;
+
+    // Constructor function that is called when the contract is deployed.
+    // It sets the "owner" variable to the address of the person who deployed the contract (msg.sender).
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // A modifier named "onlyOwner" that restricts access to certain functions.
+    // It checks if the sender (msg.sender) is the owner of the contract.
+    // If not, it throws an error with the message "You're not allowed".
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You're not allowed");
+        _; // This means "continue execution", placing the modifier in the function where it is used.
+    }
+
+   // Define a modifier named "validAddress"
+modifier validAddress() {
+    
+    // Ensure that the address calling the function (msg.sender) is not the zero address (0x0000000000000000000000000000000000000000).
+    // If msg.sender is the zero address, the transaction is reverted with the error message "Zero address not allowed".
+    require(msg.sender != address(0), "Zero address not allowed");
+    
+    // This placeholder allows the execution of the rest of the function that uses this modifier, 
+    // meaning the code in the function continues after the modifier checks are passed.
+    _;
+}
+    function createTodo(string memory _title, string memory _desc) external onlyOwner returns (bool){
+        
+    }
+}
