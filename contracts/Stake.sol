dde0ee04a8beeeea07f100f726f9b1c1b690ec3a
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-16 11:53:20 +0100

    update

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -35,7 +35,7 @@ contract stakeXFI{
     }
 
     event DepositSuccessful(address indexed staker, uint256 amount, uint256 indexed startTime);
-    event WithdrawalSuccessful(address indexed staker, uint256 amount, uint256 indexed duration);
+    event WithdrawalSuccessful(address indexed staker, uint256 amount, uint256 indexed reward);
     event OwnershipTransfered(address indexed previousOwner, address indexed newOwner);
 
     modifier onlyOwner(){
@@ -60,6 +60,95 @@ contract stakeXFI{
         require(XFIContract.balanceOf(msg.sender) >= _amount, "You don't have enough");
 
         require(XFIContract.allowance(msg.sender, address(this)) >= _amount, "Amount allowed is not enough");
+
+        XFIContract.transferFrom(msg.sender, address(this), _amount);
+
+        uint256 calReward = calculateReward(block.timestamp, _duration, _amount);
+
+        Staking memory staking;
+        staking.amount = _amount;
+        staking.duration = block.timestamp + _duration;
+        staking.startTime = block.timestamp;
+        staking.reward = calReward;
+
+        stakers[msg.sender].push(staking);
+
+        emit DepositSuccessful(msg.sender, _amount, block.timestamp);
+    }
+
+
+    function withdrawStake(uint256 _index) external reentrancyGuard returns (bool){
+        require(msg.sender != address(0), "Zero address not allowed");
+
+        require(_index < stakers[msg.sender].length, "Out of range");
+
+        Staking storage staking = stakers[msg.sender][_index];
+
+        require(block.timestamp > staking.duration, "Not yet time");
+
+        uint256 amountStaked_ = staking.amount; 
+        uint256 rewardAmount_ = staking.reward; 
+
+        staking.hasWithdraw = true;
+        staking.amount = 0;
+        staking.reward = 0;
+        staking.startTime = 0;
+        staking.duration = 0;
+
+        XFIContract.transfer(msg.sender, amountStaked_);
+        MPXContract.transfer(msg.sender, rewardAmount_);
+
+        emit WithdrawalSuccessful((msg.sender), amountStaked_, rewardAmount_);
+
+        return true;
+
+    }
+
+    function getStakerInfo(uint8 _index) external view returns(Staking memory){
+        require(msg.sender != address(0), "Zero address not allowed");
+
+        require(_index < stakers[msg.sender].length, "Out of range");
+
+        Staking memory staking = stakers[msg.sender][_index];
+        return staking;
+
+    }
+
+    function getContractXFIBalance() external onlyOwner view returns (uint256){
+        uint256 bal = XFIContract.balanceOf(address(this));
+        return bal;
+    }
+
+    function getContractMPXBalance() external onlyOwner view returns (uint256){
+        uint256 bal = MPXContract.balanceOf(address(this));
+        return bal;
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner{
+        require(_newOwner != address(0), "Zero address not allowed");
+
+        newOwner = _newOwner;
+    }
+
+    function claimOwnership() external {
+        require(msg.sender != address(0), "Zero address not allowed");
+        require(msg.sender == newOwner, "Not your turn");
+
+        emit OwnershipTransfered(owner, newOwner);
+
+        owner = newOwner;
+        newOwner = address(0);
+         
+    }
+
+    function calculateReward(uint256 _startTime, uint256 _endTime, uint256 _amount) private pure returns(uint256){
+        uint256 stakeDuration = _endTime - _startTime;
+
+        uint256 totalReward = ( REWARD_PER_SECOND * _amount * stakeDuration) / 10e11;   
+
+        return totalReward;
+
+
     }
  
 }
