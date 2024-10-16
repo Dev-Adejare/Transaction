e6c39ee90e188dd4f45ee02eb43d27bf890a78d3
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-16 10:20:20 +0100

    update

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -10,17 +10,44 @@ contract stakeXFI{
     address internal owner;
     address internal newOwner;
 
-    uint256 immutable MINIMUM_STAKE_AMOUNT;
-    uint256 immutable MAXIMUM_STAKE_AMOUNT;
+    bool internal locked;
+
+    uint256 immutable MINIMUM_STAKE_AMOUNT = 1_000 * (10**18);
+    uint256 immutable MAXIMUM_STAKE_AMOUNT = 100_000 * (10**18);
 
     uint32 constant REWARD_PER_SECOND = 1_000_000; //0.000001%
 
-    struct stake{
+    struct Staker{
         uint256 amount;
         uint256 startTime;
         uint256 duration;
         uint256 reward;
         bool hasWithdraw;
+    }
+
+    mapping (address => Staker[]) stakers;
+
+    constructor(address _xfiAddress, address _mpxAddress){
+        XFIContract = IERC20(_xfiAddress);
+        MPXContract = IERC20(_mpxAddress);
+        
+        owner = msg.sender;
+    }
+
+    event DepositSuccessful(address indexed staker, uint256 amount, uint256 indexed startTime);
+    event WithdrawalSuccessful(address indexed staker, uint256 amount, uint256 indexed duration);
+    event OwnershipTransfered(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "No Access");
+        _;
+    }
+
+    modifier reentrancyGuard(){
+        require(!locked, "Not allowed to re_enter");
+        locked = true;
+        _;
+        locked = false;
     }
 
 }
