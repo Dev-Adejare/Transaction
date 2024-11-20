351e3b3581fdeeafb90e7be573519cfaf48e3f35
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-11-20 13:47:13 +0100

    update_todo

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -1,10 +1,15 @@
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
 contract TodoList {
     address public owner;
-    enum Status {None, Created, Edited, Done}
+
+    enum Status {
+        None,
+        Created,
+        Edited,
+        Done
+    }
 
     struct Todo {
         string title;
@@ -18,76 +23,92 @@ contract TodoList {
         owner = msg.sender;
     }
 
-    modifier  onlyOwner() {
+    modifier onlyOwner() {
         require(msg.sender == owner, "You're not allowed");
         _;
     }
 
-    modifier validAddress (){
-        require(msg.sender != address(0), "Zero address not allowed");
+    modifier validAddress() {
+        require(msg.sender != address(0), "Zero Address not allowed");
         _;
     }
 
     event TodoCreated(string title, Status status);
     event TodoUpdated(string title, Status status);
-    event  TodoDeleted();
+    event TodoDeleted();
 
-    function createTodo(string memory _title, string memory _desc) external onlyOwner validAddress  returns (bool){
+    function createTodo(string memory _title, string memory _desc)
+        external
+        onlyOwner
+        validAddress
+        returns (bool)
+    {
         Todo memory mytodo;
         mytodo.title = _title;
         mytodo.description = _desc;
         mytodo.status = Status.Created;
-       
+
         todos.push(mytodo);
 
         emit TodoCreated(_title, Status.Created);
+        return true;
+    }
+
+    function updateTodo(
+        uint8 _index,
+        string memory _title,
+        string memory _desc
+    ) external onlyOwner validAddress returns (bool) {
+        require(_index < todos.length, "Out of bound");
+
+        Todo storage mytodo = todos[_index];
+        mytodo.title = _title;
+        mytodo.description = _desc;
+        mytodo.status = Status.Edited;
+
+        emit TodoCreated(_title, Status.Edited);
 
         return true;
     }
 
-    function updateTodo(uint8 _index, string memory _title, string memory _desc) external onlyOwner validAddress {
-    require(_index < todos.length, "Index is out-of-bound");
-
-    Todo storage mytodo = todos [_index];
-    mytodo.title = _title;
-    mytodo.description = _desc;
-    mytodo.status = Status.Edited;
-
-    emit TodoUpdated(_title, Status.Edited);
-
-    } 
-
-    function getTodo(uint8 _index) external view returns (string memory, string memory, Status){
-    require(_index < todos.length, "Index is out-of-bound");
-
-    Todo memory mytodo = todos[_index];
-    return (mytodo.title, mytodo.description, mytodo.status);
+    function getTodo(uint16 _index)
+        external
+        view
+        returns (
+            string memory,
+            string memory,
+            Status
+        )
+    {
+        require(_index < todos.length, "Out of bound");
+        Todo storage mytodo = todos[_index];
+        return (mytodo.title, mytodo.description, mytodo.status);
     }
 
-
-    function getAllTodo() external view returns (Todo[] memory){
+    function getAllTodo() public view returns (Todo[] memory) {
         return todos;
     }
 
-    function todoCompleted(uint8 _index) external onlyOwner validAddress returns (bool){
-        require(_index < todos.length, "Index is out-of-bound");
-
-    Todo storage mytodo = todos[_index];
-    mytodo.status = Status.Done;
-
-    return true;
-
-    }
-
-    function deleteTodo(uint8 _index) external onlyOwner validAddress{
-        require(_index < todos.length, "Index is out-of-bound");
-
+    function deleteTodo(uint16 _index) public {
+        require(_index < todos.length, "Out of bound");
         todos[_index] = todos[todos.length - 1];
         todos.pop();
-        emit TodoDeleted();
 
+        emit TodoDeleted();
+    }
+
+    function todoComplete(uint16 _index)
+        public
+        onlyOwner
+        validAddress
+        returns (bool)
+    {
+        require(_index < todos.length, "Out of bound");
+
+        Todo storage mytodo = todos[_index];
+
+        mytodo.status = Status.Done;
+
+        return true;
     }
 }
-
-
-
