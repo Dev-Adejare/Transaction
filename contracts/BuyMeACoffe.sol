53069e4d358ef6445627486d31353b6d7307e672
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-11-23 11:57:56 +0100

    update_coffee_contract

---
+++ contracts/BuyMeACoffe.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract BuyMeACoffe {
+    event NewMemo(address indexed from, uint256 timestamp, string name, uint256 amount,  string message);
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        uint256 amount;
+        string message;
+    }
+
+    address payable public owner;
+
+    Memo[] memos;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor() {
+        owner = payable (msg.sender);
+    }
+
+    function buyCoffe( string memory _name, string memory _message) public external payable {
+        require(msg.value > 0, "Cannot buy coffee for free!");
+        memos.push(Memo(msg.sender, block.timestamp, _name, msg.value, _message));
+
+        emit NewMemo(msg.sender, block.timestamp, _name, msg.value, _message);
+    }
+}
